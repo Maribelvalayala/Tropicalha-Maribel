2c0c3b49a6befc568d0041cf850fa1f56d368b70
Maribelvalayala <maribel.ayala@escola.pr.gov.br>
2025-04-01 08:50:12 -0300

    01/04

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 document.addEventListenerL('DOMContentLoaded', function(){
-const aumentarFonteBotao = document .getElementById('aumentar-fonte');
+    const aumentarFonteBotao = document .getElementById('aumentar-fonte');
+    const diminuiFonteBotao = document .getElementById('diminuir-fonte');
+     let tamanhoAtualFonte = 1;
 
-let tamanhoAtualFonte = 1;
+    aumentarFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = ${tamanhoAtualFonte}rem`;
+    });
+
+    diminuiFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = ${tamanhoAtualFonte}rem`;
+    });
 
 
-})
+});
